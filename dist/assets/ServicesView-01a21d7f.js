f09014d6746eb994058207d12e62b475365c3abe
joseluis9403 <perezmarquez9403@gmail.com>
2023-08-08 22:40:08 +0400

    adding dist

---
+++ dist/assets/ServicesView-01a21d7f.js
@@ -0,0 +1,1 @@
+import{_ as e,o as c,c as r}from"./index-279d8f83.js";const o={};function t(n,s){return c(),r("div")}const a=e(o,[["render",t]]);export{a as default};
--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,7 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/dose-engineering-website/',
   plugins: [
     vue(),
   ],
